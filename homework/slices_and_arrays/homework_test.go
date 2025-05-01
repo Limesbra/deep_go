fe232989c9ca454578edd84ff609618fec8d9e5b
karteckin.v <kartechkin.v@wildberries.work>
2025-05-01 13:52:56 +0700

    del

--- homework/slices_and_arrays/homework_test.go
+++ homework/slices_and_arrays/homework_test.go
@@ -10,70 +10,36 @@ import (
 // go test -v homework_test.go
 
 type CircularQueue struct {
-	values      []int
-	insertIndex int
-	deleteIndex int
+	values []int
+	// need to implement
 }
 
 func NewCircularQueue(size int) CircularQueue {
-	return CircularQueue{
-		values:      make([]int, size),
-		insertIndex: -1,
-		deleteIndex: -1,
-	}
+	return CircularQueue{} // need to implement
 }
 
 func (q *CircularQueue) Push(value int) bool {
-	if q.Full() {
-		return false
-	} else {
-		q.insertIndex = (q.insertIndex + 1) % len(q.values)
-		q.values[q.insertIndex] = value
-	}
-	return true
+	return false // need to implement
 }
 
 func (q *CircularQueue) Pop() bool {
-	if q.Empty() {
-		return false
-	} else {
-		q.deleteIndex = (q.deleteIndex + 1) % len(q.values)
-		if q.values[q.deleteIndex] != 0 {
-			q.values[q.deleteIndex] = 0
-		}
-	}
-	return true
+	return false // need to implement
 }
 
 func (q *CircularQueue) Front() int {
-	if q.Empty() {
-		return -1
-	}
-	return q.values[(q.deleteIndex+1)%len(q.values)]
+	return -1 // need to implement
 }
 
 func (q *CircularQueue) Back() int {
-	if q.Empty() {
-		return -1
-	}
-	return q.values[q.insertIndex]
+	return -1 // need to implement
 }
 
 func (q *CircularQueue) Empty() bool {
-	if q.insertIndex == -1 {
-		return true
-	}
-	if q.deleteIndex == q.insertIndex && q.values[q.insertIndex] == 0 {
-		return true
-	}
-	return q.insertIndex == -1
+	return false // need to implement
 }
 
 func (q *CircularQueue) Full() bool {
-	if q.deleteIndex == -1 && q.insertIndex == len(q.values)-1 {
-		return true
-	}
-	return q.deleteIndex == (q.insertIndex+1)%len(q.values) && q.values[q.deleteIndex] != 0
+	return false // need to implement
 }
 
 func TestCircularQueue(t *testing.T) {
